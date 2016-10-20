ad96f033dc96b2f67c0b88cdc3b9ec287135dcba
“Raphael” <raphaellim.creative@gmail.com>
2016-10-20 10:43:57 +0800

    sign up ui

--- dateMe/ViewController.swift
+++ dateMe/ViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
