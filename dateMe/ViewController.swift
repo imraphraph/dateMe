7ce1ecbdecb9fd462a20bd8d68a94c569867e287
“Raphael” <raphaellim.creative@gmail.com>
2016-10-20 10:13:46 +0800

    first commit

---
+++ dateMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dateMe
+//
+//  Created by Raphael Lim on 20/10/2016.
+//  Copyright © 2016 Raphael Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
