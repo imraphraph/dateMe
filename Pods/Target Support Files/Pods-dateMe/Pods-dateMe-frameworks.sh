c40f89f93958ee74b838b9e09833cc8498832d39
“Raphael” <raphaellim.creative@gmail.com>
2016-10-20 12:58:10 +0800

    added sdwebimage pod

--- Pods/Target Support Files/Pods-dateMe/Pods-dateMe-frameworks.sh
+++ Pods/Target Support Files/Pods-dateMe/Pods-dateMe-frameworks.sh
@@ -82,3 +82,12 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UIActivityIndicator-for-SDWebImage/UIActivityIndicator_for_SDWebImage.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UIActivityIndicator-for-SDWebImage/UIActivityIndicator_for_SDWebImage.framework"
+fi
